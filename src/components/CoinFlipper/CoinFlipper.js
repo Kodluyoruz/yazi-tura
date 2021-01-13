116c3798b092ebc7d29c3b8b513142e8d8d70ace
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2021-01-13 21:03:07 +0300

    Add hw version

---
+++ src/components/CoinFlipper/CoinFlipper.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Coin from "../Coin/Coin";
+import "./CoinFlipper.css";
+
+class CoinFlipper extends Component {
+  constructor(props) {
+    super(props);
+    // State üzerinde paranın başlangıçtaki durumunu veriyoruz, başlangıçta "tura" olsun.
+    // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
+    this.state = {
+      side: "tura",
+      flipping: false,
+    };
+  }
+  handleClick = () => {
+    // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
+    this.setState({ flipping: true });
+    // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
+    setTimeout(() => this.setState({ flipping: false }), 1000);
+  };
+
+  render() {
+    return (
+      <div className="CoinFlipper">
+        <h1>Yazı mı Tura mı?</h1>
+        <Coin side={this.state.side} flipping={this.state.flipping} />
+        <button onClick={this.handleClick}>At!</button>
+        <p>
+          Toplam
+          <strong> 5 </strong>
+          atıştan
+          <strong> 3 </strong>ü tura
+          <strong> 2 </strong>
+          si yazı geldi.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default CoinFlipper;
